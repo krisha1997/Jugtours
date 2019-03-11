b814e13af2134019424fbe101741d76a251885ac
samujjal1618 <samujjalsam@gmail.com>
2019-03-11 13:52:30 +0530

    Add files via upload

---
+++ src/main/java/com/assessment/developer/jugtours/model/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.assessment.developer.model;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+	Group findByName(String name);
+	List<Group> findAllByUserId(String id);
+}
---
+++ src/main/java/com/assessment/developer/jugtours/model/UserRepository.java
@@ -0,0 +1,6 @@
+package com.assessment.developer.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
